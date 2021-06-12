4482745892f8b287976a34867275e537434df920
WoLfulus <wolfulus@gmail.com>
2021-06-11 23:36:33 -0300

    force resolve registry

--- .github/actions/build-images/action.js
+++ .github/actions/build-images/action.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const dns = require('dns');
 const path = require('path');
 const core = require('@actions/core');
 const exec = require('@actions/exec');
@@ -69,6 +70,8 @@ function parseVersion(version) {
     core.endGroup();
   }
 
+  const localRegistry = (await dns.promises.resolve('registry'))[0];
+
   const imagesRoot = path.join(process.env.GITHUB_WORKSPACE || process.cwd(), 'docker');
   const images = await fs.promises.readdir(imagesRoot);
   for (const image of images.filter(image => !image.startsWith('.'))) {
@@ -80,6 +83,7 @@ function parseVersion(version) {
       '--load',
       '--tag', temp,
       '--allow=network.host',
+      '--add-host', `registry:${localRegistry}`,
       '--platform', 'linux/arm64', //'linux/amd64,linux/arm64,linux/ppc64le,linux/s390x,linux/arm/v7,linux/arm/v6',
       '--cache-to', `type=local,dest=.cache`,
       '--cache-from', `type=local,src=.cache`,
